824cfd8a7cc77660716f5ee2abb649d712a05e03
CTrotman-Dev <c.trotman92@gmail.com>
2023-08-09 21:23:22 +0100

    update contact test

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -24,12 +24,24 @@ const Form = (props) => {
 
     return (
         <div className="contact-card">
-            <form className="contact-form" onSubmit={handleSubmit}>
+            {/* <form className="contact-form" onSubmit={handleSubmit}>
                 <input onChange={handleFormChanges} value={contact.fullname} type="text" placeholder="Full name" name="fullname" />
                 <input onChange={handleFormChanges} value={contact.email} type="text" placeholder="Email" name="email" />
                 <textarea onChange={handleFormChanges} value={contact.message} name="message" placeholder="Enter message here" rows={5} cols={50} />
                 <button type="submit" className="contact-btn btn btn-lg">
                     Send
+                </button>
+            </form>
+             */}
+
+            <form action="https://send.pageclip.co/E3XGNxyvpXaW8eovvBFLD6IfNEcUZSEa" class="pageclip-form" method="post">
+
+                <input type="text" name="name" value="Roscoe Jones" />
+                <input type="email" name="email" value="roscoe@example.com" />
+                <input type="text" name="message" value="Enter Message" />
+
+                <button type="submit" class="pageclip-form__submit">
+                    <span>Send</span>
                 </button>
             </form>
         </div>
